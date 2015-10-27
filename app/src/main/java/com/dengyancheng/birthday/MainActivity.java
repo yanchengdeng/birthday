8ad07677ee94a37867873318ef8a13396965b44b
邓言诚 <yanchengdeng@gmail.com>
2015-10-27 23:27:30 +0800

    修改主配置文件

--- app/src/main/java/com/dengyancheng/birthday/MainActivity.java
+++ app/src/main/java/com/dengyancheng/birthday/MainActivity.java
@@ -5,12 +5,15 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import butterknife.ButterKnife;
+
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 
     @Override
