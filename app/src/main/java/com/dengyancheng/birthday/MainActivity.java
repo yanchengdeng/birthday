974276d6440d4495c7b650ebe2e1a97a5870944f
yanchengdeng@gmail.com <woai5jia>
2015-10-28 09:19:20 +0800

    添加生日功能

--- app/src/main/java/com/dengyancheng/birthday/MainActivity.java
+++ app/src/main/java/com/dengyancheng/birthday/MainActivity.java
@@ -1,40 +1,66 @@
 package com.dengyancheng.birthday;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-
-import butterknife.ButterKnife;
+import android.view.KeyEvent;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+	
+	private WebView webview;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		webview = (WebView) findViewById(R.id.webview);
+		
+		WebSettings ws = webview.getSettings();
+		
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+		// ws.setBuiltInZoomControls(true);
+		ws.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.NARROW_COLUMNS);
+		ws.setUseWideViewPort(true);
+		ws.setLoadWithOverviewMode(true);
+		ws.setSavePassword(true);
+		ws.setSaveFormData(true);
+		ws.setJavaScriptEnabled(true);
+		ws.setGeolocationEnabled(true);
+		ws.setGeolocationDatabasePath("/data/data/org.itri.html5webview/databases/");
+		ws.setDomStorageEnabled(true);
+		ws.setSupportZoom(true);
+		
+		webview.loadUrl("file:///android_asset/index.html");
+		
+		 Intent intent = new Intent(MainActivity.this,PlayMusicService.class);
+         startService(intent);
+	}
+	
+	private long mExitTime;
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		if (keyCode == KeyEvent.KEYCODE_BACK) {
+				if ((System.currentTimeMillis() - mExitTime) > 2000) {
+					Toast.makeText(this, "再按一次退出", Toast.LENGTH_SHORT).show();
+					mExitTime = System.currentTimeMillis();
+				} else {
+				finish();
+				}
+			}
+			return true;
+	}
+	
+	
+	@Override
+	protected void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
+		 Intent intent = new Intent(MainActivity.this,PlayMusicService.class);
+         stopService(intent);
+	}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
---
+++ app/src/main/java/com/dengyancheng/birthday/PlayMusicService.java
@@ -0,0 +1,58 @@
+package com.dengyancheng.birthday;
+
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.media.MediaPlayer;
+
+public class PlayMusicService extends IntentService {
+	
+	private Thread myThread;
+	 private MediaPlayer mp;
+	public PlayMusicService(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public PlayMusicService() {
+		// TODO Auto-generated constructor stub
+		super("PlayMusicService");
+	}
+
+	@Override
+	protected void onHandleIntent(Intent intent) {
+		// TODO Auto-generated method stub
+		mp =MediaPlayer.create(this, R.raw.love);
+		mp.setLooping(true);
+		myThread  = new MyThread();
+		myThread.start();
+		}
+	
+	private class MyThread extends Thread
+    {
+		@Override
+		public void run() {
+			try
+			{
+			if (mp!=null)
+			{
+				mp.stop();
+			}
+			mp.prepare();
+			mp.start();
+			} catch (Exception e)
+			{
+			e.printStackTrace();
+			}
+			super.run();
+		}
+		
+    }
+	
+	@Override
+	public void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
+	}
+
+}
